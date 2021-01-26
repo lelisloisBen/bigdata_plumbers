a87f911843aadd93e46aa6ba1f726f1a7277c9c7
lelisloisBen <samirbenzada@gmail.com>
2021-01-26 12:12:43 -0500

    update

--- 0001_FreddieMac/BYOL_Reg.py
+++ 0001_FreddieMac/BYOL_Reg.py
@@ -14,20 +14,7 @@ df_sales100=spark.read.csv("s3://freddie-mac-sandbox-raw/byol/byol_sales_1.datas
 # Adding a Timestamp column for the parquet file
 df_final=df_sales100.withColumn('edptimestamp', F.current_timestamp())
 
-# counting row after adding Timestamps
-a=df_final.count()
-
-# Printing the count
-print(f'Staging count is :{a}')
-
 # Writing the parquet file to S3 raw-plus Bucket
 df_final.write.format("parquet").mode("overwrite").save(s3_out_uri)
 
-# Reading the parquet file after writing it
-df_read_s3=spark.read.parquet(s3_out_uri)
-
-# Counting the parquet file from row-plus Bucket
-b=df_read_s3.count()
-
-# Print Final count of parquet file
-print(f'Final count is :{b}')
+print("New Dataset Uploaded")
